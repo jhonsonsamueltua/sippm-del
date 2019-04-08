44f0da463afebf6c05af28d16c5ab8a6a7c13ed5
Alexander N. Salim <alexandernsalim@gmail.com>
2019-04-08 11:59:21 +0700

    Fix error when merging branch

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -35,34 +35,4 @@ $css = ['css/site.css'];
         ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
         fugiat nulla pariatur.</p>
     </div>
-           
-    <div class="col-md-4">
-        <div class="panel panel-primary" style= "background-color:#193b52;">
-            <div class="panel-heading" style= "background-color:#193b52;">
-                <i class='fa fa-file-text-o fa-fw'></i> Semua Repository
-            </div>
-            <a href="sejarah.html" class="list-group-item">Judul</a>
-            <a href="fasilitas.html" class="list-group-item">Jenis</a>
-            <a href="visi.html" class="list-group-item">Dosen Penugas</a>
-            <a href="prestasi.html" class="list-group-item">Tahun Ajaran</a>
-        </div>
-        <div class="panel panel-primary" style= "background-color:#193b52;">
-            <div class="panel-heading" style= "background-color:#193b52;">
-                <i class='fa fa-file-text-o fa-fw'></i> Judul
-            </div>
-            <a href="sejarah.html" class="list-group-item">Sejarah</a>
-            <a href="fasilitas.html" class="list-group-item">Fasilitas</a>
-            <a href="visi.html" class="list-group-item">Visi Misi</a>
-            <a href="prestasi.html" class="list-group-item">Prestasi</a>
-        </div>
-        <div class="panel panel-primary" style= "background-color:#193b52;">
-            <div class="panel-heading" style= "background-color:#193b52;">
-                <i class='fa fa-file-text-o fa-fw'></i> Jenis
-            </div>
-            <a href="sejarah.html" class="list-group-item">Sejarah</a>
-            <a href="fasilitas.html" class="list-group-item">Fasilitas</a>
-            <a href="visi.html" class="list-group-item">Visi Misi</a>
-            <a href="prestasi.html" class="list-group-item">Prestasi</a>
-        </div>
-    </div>
 </div>
